c6836cb0828556fd2ddfb057f1e92fad87de3cf2
Joao Marcos Souza Fernandes <jo0209.letras@gmail.com>
2025-01-13 17:52:44 -0300

    Changing everything again :)

--- EzProject/EzProject.cpp
+++ EzProject/EzProject.cpp
@@ -2,26 +2,54 @@
 #include <wx/wx.h>
 #include <wx/frame.h>
 #include <wx/app.h>
+#include <dwmapi.h>
 
 class MainFrame : public wxFrame {
 
 public:
 
-	MainFrame(const char* title) : wxFrame (nullptr, wxID_ANY, title) {
+	MainFrame(const char* title) : wxFrame (nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE & ~wxRESIZE_BORDER) {
 
-		wxBoxSizer* mainsizer = new wxBoxSizer(wxVERTICAL);
-		wxPanel* mainpanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(350, 500));
+		this->SetBackgroundColour(bgColor);
 
-		wxBoxSizer* mainpanel_sizer = new wxBoxSizer(wxVERTICAL);
+		wxPanel* panel_main = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(300, 400));
+		panel_main->SetBackgroundColour(bgColor);
 
-		wxStaticText* l_projectName = new wxStaticText(mainpanel, wxID_ANY, "Project name: ");
+		wxStaticText* label_projectName = new wxStaticText(panel_main, wxID_ANY, "Project name");
+		wxTextCtrl* input_projectName = new wxTextCtrl(panel_main, wxID_ANY);
 
-		mainsizer->Add(mainpanel, 1, wxEXPAND);
-		this->SetSizerAndFit(mainsizer);
+		wxStaticText* label_projectDirectory = new wxStaticText(panel_main, wxID_ANY, "Project directory");
+		wxTextCtrl* input_projectDirectory = new wxTextCtrl(panel_main, wxID_ANY);
+		wxButton* button_projectDirectory = new wxButton(panel_main, wxID_ANY, "...", wxDefaultPosition, wxSize(25, -1));
 
+		wxBoxSizer* sizer_projectDirectory = new wxBoxSizer(wxHORIZONTAL);
+		sizer_projectDirectory->Add(input_projectDirectory, 1, wxRIGHT, 5);
+		sizer_projectDirectory->Add(button_projectDirectory);
+
+		wxButton* button_createProject = new wxButton(panel_main, wxID_ANY, "Create");
+
+		wxBoxSizer* sizer_main = new wxBoxSizer(wxVERTICAL);
+		sizer_main->Add(label_projectName);
+		sizer_main->AddSpacer(4);
+		sizer_main->Add(input_projectName, 0, wxEXPAND);
+		sizer_main->AddSpacer(8);
+		sizer_main->Add(label_projectDirectory);
+		sizer_main->AddSpacer(4);
+		sizer_main->Add(sizer_projectDirectory, 0, wxEXPAND);
+		sizer_main->AddStretchSpacer();
+		sizer_main->Add(button_createProject, 0, wxALIGN_RIGHT | wxRIGHT | wxTOP, 5);
+		panel_main->SetSizer(sizer_main);
+
+		wxBoxSizer* sizer_global = new wxBoxSizer(wxVERTICAL);
+		sizer_global->Add(panel_main, 0, wxALL, 8);
+		this->SetSizerAndFit(sizer_global);
+
+		this->Center();
 	}
 
 private:
+
+	const wxColour bgColor = *wxWHITE;
 
 };
 
